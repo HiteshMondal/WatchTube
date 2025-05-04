b96888a8b5d61b272b733f44a53e4553c890c8d5
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-05-04 21:57:06 +0530

    Changes to be committed: 	new file:   app/(tabs)/_layout.tsx 	new file:   app/(tabs)/index.tsx 	new file:   app/(tabs)/profile.tsx 	new file:   app/(tabs)/saved.tsx 	new file:   app/(tabs)/search.tsx 	modified:   app/_layout.tsx 	new file:   app/movies/[id].tsx 	new file:   app/onboarding.tsx 	new file:   assets/icons/arrow.png 	new file:   assets/icons/back_arrow_icon.png 	new file:   assets/icons/bell.png 	new file:   assets/icons/caret.png 	new file:   assets/icons/facebook.png 	new file:   assets/icons/github.jpg 	new file:   assets/icons/home.png 	new file:   assets/icons/info.png 	new file:   assets/icons/linkedin.png 	new file:   assets/icons/logo.png 	new file:   assets/icons/play.png 	new file:   assets/icons/profile.png 	new file:   assets/icons/save.png 	new file:   assets/icons/search.png 	new file:   assets/images/background_banner.png 	new file:   assets/images/background_banner1.png 	new file:   assets/images/birds.jpg 	new file:   assets/images/highlight.png 	new file:   assets/images/logo.png 	new file:   assets/images/media player.jpg 	new file:   assets/images/ocean.png 	new file:   assets/images/rankingGradient.png 	new file:   assets/images/star.jpg 	new file:   constants/icons.ts 	new file:   constants/images.ts 	new file:   interfaces/interfaces.d.ts  Changes not staged for commit: 	modified:   babel.config.js 	modified:   constants/icons.ts 	modified:   constants/images.ts 	modified:   tailwind.config.js 	modified:   tsconfig.json  Untracked files: 	assets.d.ts 	assets/icons/person.png 	assets/icons/star.png

--- constants/images.ts
+++ constants/images.ts
@@ -1,9 +1,9 @@
 import bg from "@/assets/images/background_banner.png";
 import highlight from "@/assets/images/highlight.png";
-import rankingGradient from "@/assets/images/ranking_gradient.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
 
 export const images = {
-    bg,
-    highlight,
-    rankingGradient,
+  bg,
+  highlight,
+  rankingGradient,
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {
-      colors:{
-        primary: '#030012',
-        accent: 'darkviolet',
-        light: {
-          100: '#e0e0e0',
-          200: '#c2c2c2',
-          300: '#a3a3a3',
-        },
-        dark: {
-          100: '#333333',
-          200: '#1a1a1a',
-          300: '#000000',
-        }
-
+export const content = ["./app/**/*.{js,jsx,ts,tsx}"];
+export const presets = [require("nativewind/preset")];
+export const theme = {
+  extend: {
+    colors: {
+      primary: '#030012',
+      accent: 'darkviolet',
+      light: {
+        100: '#e0e0e0',
+        200: '#c2c2c2',
+        300: '#a3a3a3',
+      },
+      dark: {
+        100: '#333333',
+        200: '#1a1a1a',
+        300: '#000000',
       }
-    },
+    }
   },
-  plugins: [],
-}       
+};
+export const plugins = [];       
