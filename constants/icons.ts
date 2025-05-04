f9d03b309cdd0bebff332b0d69a093cfc0070533
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-05-04 21:55:14 +0530

    Changes to be committed: 	new file:   app/(tabs)/_layout.tsx 	new file:   app/(tabs)/index.tsx 	new file:   app/(tabs)/profile.tsx 	new file:   app/(tabs)/saved.tsx 	new file:   app/(tabs)/search.tsx 	modified:   app/_layout.tsx 	new file:   app/movies/[id].tsx 	new file:   app/onboarding.tsx 	new file:   assets/icons/arrow.png 	new file:   assets/icons/back_arrow_icon.png 	new file:   assets/icons/bell.png 	new file:   assets/icons/caret.png 	new file:   assets/icons/facebook.png 	new file:   assets/icons/github.jpg 	new file:   assets/icons/home.png 	new file:   assets/icons/info.png 	new file:   assets/icons/linkedin.png 	new file:   assets/icons/logo.png 	new file:   assets/icons/play.png 	new file:   assets/icons/profile.png 	new file:   assets/icons/save.png 	new file:   assets/icons/search.png 	new file:   assets/images/background_banner.png 	new file:   assets/images/background_banner1.png 	new file:   assets/images/birds.jpg 	new file:   assets/images/highlight.png 	new file:   assets/images/logo.png 	new file:   assets/images/media player.jpg 	new file:   assets/images/ocean.png 	new file:   assets/images/rankingGradient.png 	new file:   assets/images/star.jpg 	new file:   constants/icons.ts 	new file:   constants/images.ts 	new file:   interfaces/interfaces.d.ts  Changes not staged for commit: 	modified:   babel.config.js 	modified:   constants/icons.ts 	modified:   constants/images.ts 	modified:   tailwind.config.js 	modified:   tsconfig.json  Untracked files: 	assets.d.ts 	assets/icons/person.png 	assets/icons/star.png

---
+++ constants/icons.ts
@@ -0,0 +1,9 @@
+import 
+
+export const icons = {
+    home: require('../assets/icons/home.png'),
+    search: require('../assets/icons/search.png'),
+    saved: require('../assets/icons/saved.png'),
+    profile: require('../assets/icons/profile.png'),
+    
+}
