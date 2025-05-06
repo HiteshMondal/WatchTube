6ac0e132f8f0e36659dbe225c451c85124ebf070
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-05-06 21:25:46 +0530

    new file:   .env 	modified:   app/(tabs)/index.tsx 	new file:   components/SearchBar.tsx 	new file:   services/api.ts 	new file:   services/useFetch.ts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView } from "react-native";
+import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+
+  const router = useRouter();
+
   return (
     <SafeAreaView className="flex-1">
       <Image source={images.bg} className="absolute size-full z-0" />
@@ -13,7 +18,14 @@ export default function Index() {
         contentContainerStyle={{ paddingBottom: 50 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-4 mb-5 mx-auto" />
-        {/* Add content here */}
+        <View className="flex-1 mt-5">
+          <SearchBar 
+            onPress={() => {
+              router.push('/search')
+            }}
+            placeholder="Search!"
+          />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
