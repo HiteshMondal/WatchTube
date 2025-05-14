a60da9b83454d9d3f064fc5f4b58ec86e5f60f50
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-05-14 21:31:11 +0530

    modified:   app/tabs/_layout.tsx

--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -66,14 +66,6 @@ const _layout = () => {
           tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.save} title="Saved" />
         }}
       />
-      <Tabs.Screen 
-        name="profile" 
-        options={{ 
-          title: "Profile",
-          headerShown: false,
-          tabBarIcon: ({ focused }) => <TabIcon focused={focused} icon={icons.person} title="Profile" />
-        }}
-      />
     </Tabs>
   );
 }
